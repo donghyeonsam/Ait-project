8721f555a05f03ce897af92f7f04bb0ebc1955a5
minbeom <alsqja104@gmail.com>
2026-07-28 15:48:05 +0900

    feat: 그룹 신청, 승인, 거절

---
+++ be/src/main/java/com/aitserver/studyGroupRoom/dto/ApplicationProcessRequest.java
@@ -0,0 +1,12 @@
+package com.aitserver.studyGroupRoom.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class ApplicationProcessRequest {
+    @JsonProperty("isApprove")
+    private boolean isApprove; // true: 승인, false: 거절
+}
