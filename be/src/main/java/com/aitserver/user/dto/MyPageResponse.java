b2edcb071b934263fe1bbb1e95e4c7bc9a3e6611
박승진 <park00seung@gmail.com>
2026-08-05 15:55:37 +0900

    fix: 프로필 이미지 조회 url 수정

--- be/src/main/java/com/aitserver/user/dto/MyPageResponse.java
+++ be/src/main/java/com/aitserver/user/dto/MyPageResponse.java
@@ -4,10 +4,12 @@ package com.aitserver.user.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
 @Getter
+@Setter
 @Builder
 @AllArgsConstructor
 public class MyPageResponse {
